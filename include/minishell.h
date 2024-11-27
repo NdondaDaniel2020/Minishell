991464842fa7baf419b0e6e9fc52204139f2071e
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2024-11-27 09:56:48 +0100

    prototipagens declaradas

--- include/minishell.h
+++ include/minishell.h
@@ -98,5 +98,6 @@ int		ft_strnchrcmp(const char *s1, const char *s2, size_t n, char chr);
 void	add_environment_variable(char *env_var, t_data *data);
 
 char	*get_env(char *env, t_data *data);
-
+void	change_environment_variables_question_mark(int value, t_data *data);
+int	len_matrix(char **matrix);
 #endif
