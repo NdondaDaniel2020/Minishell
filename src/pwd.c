4bf6ba9afcf673eb2e24ed5849e19dd86df63b40
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2024-12-02 11:23:33 +0100

    Ajusto pwd

--- src/pwd.c
+++ src/pwd.c
@@ -17,12 +17,16 @@ void	pwd(t_data *data)
 	int		i;
 	char	*cwd;
 
-	i = len_matrix(data->btree->content);
-	if (i > 1)
+	i = 0;
+	while (data->btree->content[i])
 	{
-		write(2, "pwd: too many arguments\n", 24);
-		change_environment_variables_question_mark(1, data);
-		return ;
+		if (i > 0 && ft_strlen(data->btree->content[i]) > 0)
+		{
+			write(2, "pwd: too many arguments\n", 24);
+			change_environment_variables_question_mark(1, data);
+			return ;
+		}
+		i++;
 	}
 	cwd = ft_calloc(5048, sizeof(char));
 	if (!cwd)
@@ -30,10 +34,7 @@ void	pwd(t_data *data)
 		change_environment_variables_question_mark(2, data);
 		return ;
 	}
-	if (getcwd(cwd, 5048) == NULL)
-	{
-		free(cwd);
-	}
+	getcwd(cwd, 5048);
 	ft_printf("%s\n", cwd);
 	free(cwd);
 	change_environment_variables_question_mark(0, data);
