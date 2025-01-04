afa8f8db151b9a008341ef465177ac4608c0ae68
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2025-01-04 21:14:30 +0100

    base da execucao do redirecionamento feito

--- src/main.c
+++ src/main.c
@@ -12,117 +12,83 @@
 
 #include "minishell.h"
 
-void	insert_data(t_data *data, char *command)
+static int	count_file_redirection(t_new_list *aux)
 {
-	int		i;
-	char	**spliting;
+	int 	i;
+	int		len;
+	bool	valid;
 
 	i = 0;
-	data->command = command;
-	spliting = split_2(command, '|');
-	while (spliting[i])
+	len = 0;
+	valid = false;
+	while (aux->content[i])
 	{
-		ft_lstnew_addfront(&data->list,
-			ft_lstnew_new(split_2(spliting[i], ' ')));
-		free(spliting[i]);
+		if (valid_string_condition_for_redirection(aux->content[i])
+			&& valid == false)
+			valid = true;
+		if (valid)
+			len++;
 		i++;
 	}
-	free(spliting);
+	return (len);
 }
 
-void	execute_command(int i, t_new_list *aux, t_data *data)
+void	split_redirect_between_file_and_content(t_new_list *aux, t_data *data)
 {
-	if (!ft_strncmp(aux->content[i], "exit", ft_strlen(aux->content[i])))
-		exit_(aux, data);
-	else if (!ft_strncmp(aux->content[i], "pwd", ft_strlen (aux->content[i])))
-		pwd(aux, data);
-	else if (!ft_strncmp(aux->content[i], "cd", ft_strlen(aux->content[i])))
-		cd(aux, data);
-	else if (!ft_strncmp(aux->content[i], "echo", ft_strlen(aux->content[i])))
-		echo(aux, data);
-	else if (!ft_strncmp(aux->content[i], "env", ft_strlen(aux->content[i])))
-		env(aux, data);
-	else if (!ft_strncmp(aux->content[i], "export", ft_strlen(aux->content[i])))
-		export(aux, data);
-	else if (!ft_strncmp(aux->content[i], "unset", ft_strlen(aux->content[i])))
-		unset(data);
-	else
-		other_command(i, aux, data);
-}
-
-void	redirection(int i, t_new_list *aux, t_data *data)
-{
-	int		it;
+	int 	i;
+	int		rm;
 	int		len;
+	int		len_m;
+	bool	valid;
+
+	i = 0;
+	rm = 0;
+	valid = false;
+	len_m = len_matrix(aux->content);
+	len = count_file_redirection(aux);
+	data->redirection_matrix = ft_calloc(len + 1, sizeof(char *));
+	while (i < len_m)
+	{
+		if (valid_string_condition_for_redirection(aux->content[i])
+			&& valid == false)
+			valid = true;
+		if (valid)
+		{
+			data->redirection_matrix[rm++] = aux->content[i];
+			aux->content[i] = NULL;
+		}
+		i++;
+	}
+}
+
+void	redirection(t_new_list *aux, t_data *data)
+{
 	char	**new_content;
 
-	(void)i;
 	new_content = reset_the_array_for_redirection(aux->content);
 	if (new_content)
 	{
-		ft_lstnew_addafter_pos(&data->list, data->list, ft_lstnew_new(new_content));
+		ft_lstnew_addafter_pos(&data->list, data->list,
+			ft_lstnew_new(new_content));
 		return ;
 	}
-	
-	//////////////////////////////////////////////////////////////////////////////////////	
-	len = len_matrix(aux->content);
-	//////////////////////////////////////////////////////////////////////////////////////
-
-	if ((valid_string_condition_for_redirection(aux->content[len - 1]))
-		|| ((ft_strlen(aux->content[len - 1]) == 0) && valid_string_condition_for_redirection(aux->content[len - 2])))
-	{
-		ft_putstr_fd("syntax error near unexpected token `newline'\n", 2);
+	if (valid_redirection_syntax(aux))
 		return ;
-	}
-	
-	//////////////////////////////////////////////////////////////////////////////////////
-	
-	it = 0;
-	while (aux->content[it])
-	{	
-		if (valid_string_condition_for_redirection(aux->content[it]) && is_directory_valid(aux->content[it + 1]))
-		{
-			ft_putstr_fd(aux->content[it + 1], 2);
-			ft_putstr_fd(": Is a directory\n", 2);
-			return ;
-		}
-		it++;
-	}
-
-	//////////////////////////////////////////////////////////////////////////////////////
-
-	if ((is_directory_valid(aux->content[len - 1]))
-		|| ((ft_strlen(aux->content[len - 1]) == 0) && is_directory_valid(aux->content[len - 2])))
-	{
-		ft_putstr_fd("syntax error near unexpected token `newline'\n", 2);
+	if (adjust_filename_in_redirection_syntax(aux))
 		return ;
-	}
-	
-	//////////////////////////////////////////////////////////////////////////////////////
-
-	it = 0;
 	ajust_all_position(&aux->content);
-	while (aux->content[it])
-	{
-		ft_printf("[%s]", aux->content[it]);
-		it++;
-	}
+	split_redirect_between_file_and_content(aux, data);
 
-	ft_printf("\n");
-
-	//3 - Tratamento
-		/// se tiver espanco em algum lugar do nome do arquivo manter
-		///  >> test/"test muito estranho"/a2.txt
-		///  >> test/test\ muito\ estranho/a2.txt
-		///  >> test/"   a2.txt  "
-		///  >> test/"""a2.txt"""
-		///  >> test/""''a2.txt''""
-		///  >> test/''""a2.txt""''
-		/// se tiver ""' file '"" -> file 
-		/// ->> retire apenas as chavetar que podem ser ' ou " e o resto vai no file
-		/// not / & ! $ ( ) > < >> << error -> syntax error near unexpected token `caracter'
-
-	// ...DEPOIS DE CADA (>, <, >>, <<, <>) REDIRECIONAMENTO FUNCIONAR SOZINHO 
+	int	len_m = len_matrix(data->redirection_matrix);
+	if (ft_strncmp(data->redirection_matrix[len_m - 2], ">>", 2) == 0)
+		output_append(data, aux);
+	else if (ft_strncmp(data->redirection_matrix[len_m - 2], "<<", 2) == 0)
+		ft_putstr_fd("opcao ainda nao desenvolvida\n", 2);
+	else if (ft_strncmp(data->redirection_matrix[len_m - 2], "<", 1) == 0)
+		input(data, aux);
+	else if (ft_strncmp(data->redirection_matrix[len_m - 2], ">", 1) == 0)
+		output(data, aux);
+	// ...DEPOIS DE CADA (>, <, >>, <<, <>) REDIRECIONAMENTO FUNCIONAR SOZINHO
 	// ESTUDAR OS REDIRECIONAMENTO COMHINADO 
 }
 
@@ -146,7 +112,7 @@ void	master(char *command, t_data *data)
 		if (ft_strlen(aux->content[i]) == 0)
 			i++;
 		if (value_redirection == 1)
-			redirection(i, aux, data);
+			redirection(aux, data);
 		else if (value_redirection == 0)
 			execute_command(i, aux, data);
 		aux = aux->next;
@@ -154,7 +120,7 @@ void	master(char *command, t_data *data)
 	free_all_data(data);
 }
 
-int	main(void) //  test/test muito estranho/  --> saida --> test/test muito estranho/: Is a directory
+int	main(void)
 {
 	t_data	data;
 	char	*input;
