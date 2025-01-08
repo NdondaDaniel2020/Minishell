4104b0001c9db6c7dbf76d6b4b060322ca1abb08
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2025-01-08 08:23:38 +0100

    Ajuste da norma

--- src/redirection_util_7.c
+++ src/redirection_util_7.c
@@ -15,9 +15,9 @@
 static bool	condition_count_redict(int pos, int len, char chr, char *str)
 {
 	return ((pos == 0 && ((pos + 1 < len && str[pos + 1] != chr)
-			|| ((pos + 1 < len && str[pos + 1] == chr)
-			&& (pos + 2 < len && str[pos + 2] != chr))))
-			|| pos != 0);
+				|| ((pos + 1 < len && str[pos + 1] == chr)
+					&& (pos + 2 < len && str[pos + 2] != chr))))
+		|| pos != 0);
 }
 
 int	str_in_list_redirection(char *str, int len_m)
@@ -35,7 +35,7 @@ int	str_in_list_redirection(char *str, int len_m)
 		if (ft_strncmp(str + pos, list[i], ft_strlen(list[i])) == 0)
 		{
 			if (len == 2 && str[0] == '<' && str[1] == '>')
-				return (len_m);		
+				return (len_m);
 			if (pos != 0 && ((pos + 1 < len && str[pos + 1] != list[i][0])
 				|| ((pos + 1 < len && str[pos + 1] == list[i][0])
 				&& (pos + 2 < len && str[pos + 2] != list[i][0]))))
