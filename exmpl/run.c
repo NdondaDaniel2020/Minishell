ae02d7fa94787a17da4b4a4b66882d2cdb87bfb8
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2025-01-13 22:46:17 +0100

    expancao de variavel feito, falta ajustar a norma e criar o o corpo principar da funcao

--- exmpl/run.c
+++ exmpl/run.c
@@ -12,6 +12,43 @@
 
 #include "run.h"
 
+char	*substring(const char *str, int start, int end)
+{
+	int 	len;
+	char	*sub;
+
+	len = strlen(str);
+	if (start > end)
+		return (NULL);
+	if (start < 0)
+		start = 0;
+	if (end > len)
+		end = len;
+	sub = (char *)malloc((end - start + 1) * sizeof(char));
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, str + start, end - start + 1);
+	sub[end - start] = '\0';
+	return (sub);
+}
+
+///////////
+char	*invert_str(char *str)
+{
+	int		i;
+	int		len;
+	char	*new_string;
+
+	if (!str)
+		return (NULL);
+	i = 0;
+	len = ft_strlen(str);
+	new_string = ft_calloc(len + 1, sizeof(char));
+	while (str[i])
+		new_string[len-- - 1] = str[i++];
+	return (new_string);
+}
+
 bool	condition_put_env_quotes(char *str, char *sub) // """$USER"""
 {
 	return (first_str('\'', str) && (count_chr('\'', sub) % 2 != 0)
@@ -39,108 +76,180 @@ bool	condition_extract_value_env(char *str, char *sub) // name
 		 || (first_str('\'', str) && (count_chr('\'', sub) % 2 == 0)
 		 	&& (count_chr('"', sub) > 0)));
 }
+///////////
 
-static void	extract_value_env(int i, t_new_list *aux, t_data *data)
+///////////
+
+char	*extract_value_env(int i, char *str, t_data *data)
 {
-	char	*value_env;
-	char	*aux_env;
-
-	aux_env = ft_strtrim(aux->content[0] + i, "\"'");
-	value_env = get_env(aux_env + 1, data);
-	// so add value_env in matrix os comportamentos abaaixo o codigo resolve
-
-	free(aux_env);
-}
-
-void	extract_value_env_quotes(int i, char *str, t_data *data)
-{
-	char	*value_env;
-	char	*aux_env;
-	char	*mark;
-	char	*cpy_env;
-
-	aux_env = ft_strtrim(str, "\"'");
-	value_env = get_env(aux_env + 1, data);
-	if (value_env)
-	{
-		cpy_env = ft_strdup(value_env);
-		mark = ft_strtrim(str + (i + ft_strlen(aux_env)), "\"");
-		value_env = ft_strjoin(mark, value_env);
-		value_env = ft_strjoin_free(value_env, mark);
-		ft_printf("%s\n", value_env);
-		// so add value_env in matrix os comportamentos abaaixo o codigo resolve
-	}
-	free(value_env);
-	free(aux_env);
-	free(cpy_env);
-	free(mark);	
-}
-
-// void	check_environment_variable_expansion(char **str, t_data *data)
-
-void	environment_variation_expansion(int i, char ***matrix, t_data *data)
-{
-	int 	ii;
 	char	*sub;
-	char	*value_env;
 
-	ii = 0;
-	while ((*matrix)[i][ii] && (*matrix)[i][ii] != '$')
-		ii++;
-
-	sub = ft_substr((*matrix)[i], 0, ft_strchr((*matrix)[i], '$') - (*matrix)[i]);
-	if (condition_put_env((*matrix)[i], sub))
-	{	
-		int	old_size;
-		int	new_size;
-
-		value_env = ft_strtrim((*matrix)[i], "'\"");
-		ft_printf("[%s]\n", value_env);
-
-		old_size = ft_strlen((*matrix)[i]);
-		new_size = ft_strlen(value_env);
-		(*matrix)[i] = ft_realloc((*matrix)[i], old_size, new_size);
-		ft_strlcpy((*matrix)[i], value_env, new_size);
-		free(value_env);
-	}
-	else if (condition_extract_value_env((*matrix)[i], sub)) // V/
+	sub = ft_substr(str, 0, ft_strchr(str, '$') - str);
+	if (condition_put_env(str, sub))
 	{
 		int 	pos;
+		int		end;
 		char	*aux;
-		int		old_size;
-		int		new_size;
+		char	*value_env;
 
-		value_env = ft_strtrim((*matrix)[i], "'\"");
+		value_env = ft_strtrim(str, "'\"");
 		pos = 0;
 		while (value_env[pos] && value_env[pos] == '$')
 			pos++;
-		aux = get_env(value_env + pos, data);
-		old_size = ft_strlen((*matrix)[i]);
-		new_size = ft_strlen(aux);
-		(*matrix)[i] = ft_realloc((*matrix)[i], old_size, new_size);
-		ft_strlcpy((*matrix)[i], aux, new_size);
+		end  = pos + 1;
+		while (value_env[end] && value_env[end] != '$'
+			&& value_env[end] != '\'' && value_env[end] != '"')
+			end++;
 		free(value_env);
+		aux = substring(str, 0, end);
+		free(sub);
+		return (aux);
 	}
-	else if (condition_extract_value_env_quotes((*matrix)[i], sub))
-	{	
-		// ft_printf("'''name'''\n");
-		// extract_value_env_quotes(i, (*matrix)[i], data);
-	}	
-	else if (condition_put_env_quotes((*matrix)[i], sub))
+	else if (condition_extract_value_env(str, sub)) // V/
 	{
-		int	old_size;
-		int	new_size;
+		int 	pos;
+		int		end;
+		char	*aux;
+		char	*value_env;
 
-		value_env = ft_strtrim((*matrix)[i], "'");
-		ft_printf("[%s]\n", value_env);
-
-		old_size = ft_strlen((*matrix)[i]);
-		new_size = ft_strlen(value_env);
-		(*matrix)[i] = ft_realloc((*matrix)[i], old_size, new_size);
-		ft_strlcpy((*matrix)[i], value_env, new_size);
+		value_env = ft_strtrim(str, "'\"");
+		pos = 0;
+		while (value_env[pos] && value_env[pos] == '$')
+			pos++;
+		end  = pos + 1;
+		while (value_env[end] && value_env[end] != '$'
+			&& value_env[end] != '\'' && value_env[end] != '"')
+			end++;
 		free(value_env);
+		aux = substring(str, 0, end);
+		pos = 0;
+		while (aux[pos] && aux[pos] == '$')
+			pos++;
+		value_env = ft_strdup(get_env(aux + pos, data));
+		free(sub);
+		return (value_env);
 	}
-	free(sub);
+	else if (condition_extract_value_env_quotes(str, sub))
+	{
+		int 	end;
+		int 	start;
+		char	*aux;
+		char	*sub_str;
+		char	*inv_sub;
+
+		start = 0;
+		while (str[start] != '$')
+			start++;
+		end = start + 1;
+		while (ft_isalpha(str[end]))
+			end++;
+		aux = ft_strtrim(sub, "\"");
+		inv_sub = invert_str(aux);
+		free(aux);
+		sub_str = substring(str, start, end);
+		aux = ft_strdup(get_env(sub_str + 1, data));
+		free(sub_str);
+		sub_str = ft_strjoin(inv_sub, aux);
+		free(aux);
+		aux = ft_strjoin_free(sub_str, inv_sub);
+		free(inv_sub);
+		free(sub);
+		return (aux);
+	}
+	else if (condition_put_env_quotes(str, sub))
+	{
+		int 	end;
+		int 	start;
+		char	*aux;
+		char	*sub_str;
+		char	*inv_sub;
+
+		start = 0;
+		while (str[start] != '$')
+			start++;
+		end = start + 1;
+		while (ft_isalpha(str[end]))
+			end++;
+		aux = ft_strtrim(sub, "'");
+		inv_sub = invert_str(aux);
+		free(aux);
+		sub_str = substring(str, start, end);
+		aux = ft_strdup(sub_str);
+		free(sub_str);
+		sub_str = ft_strjoin(inv_sub, aux);
+		free(aux);
+		aux = ft_strjoin_free(sub_str, inv_sub);
+		free(inv_sub);
+		free(sub);
+		return (aux);
+	}
+	return (NULL);
+}
+
+///////////
+
+
+
+
+
+// void	check_environment_variable_expansion(char **str, t_data *data)
+
+static int	adjust_position_variation(int pos, char *sub, char *str, t_data *data)
+{
+	int len;
+
+	len = ft_strlen(str);
+
+	if (pos == 0)
+		pos = ft_strlen(sub);
+	if (str[pos] == '$')
+		pos++;
+	while ((pos < len) && str[pos] != '\''
+		&& str[pos] != '"' && str[pos] != '$')
+		pos++;
+	pos += ft_strlen(sub);
+	return (pos);
+}
+
+char	*get_environment_variation_expansion(int i, char ***matrix, t_data *data)
+{
+	int		len;
+	int		pos;
+	char	*sub;
+	char	*join;
+	char	*value_env;
+
+	pos = 0;
+	join = NULL;
+	len = ft_strlen((*matrix)[i]);
+	while (pos < len)
+	{
+		value_env = extract_value_env(i, (*matrix)[i] + pos, data);
+		sub = ft_substr(((*matrix)[i] + pos), 0,
+			ft_strchr(((*matrix)[i] + pos), '$') - ((*matrix)[i] + pos));
+		pos += adjust_position_variation(pos, sub, (*matrix)[i], data);
+		if (join == NULL)
+			join = ft_strjoin_free(value_env, join);
+		else
+			join = ft_strjoin_free(join, value_env);
+		free(sub);
+	}
+	free(value_env);
+	return (join);
+}
+
+void	environment_variation_expansion(int i, char ***matrix, t_data *data)
+{
+	int		old_size;
+	int		new_size;
+	char	*value_env;
+
+	value_env = get_environment_variation_expansion(i, matrix, data);
+	old_size = ft_strlen((*matrix)[i]);
+	new_size = ft_strlen(value_env);
+	(*matrix)[i] = ft_realloc((*matrix)[i], old_size, new_size + 1);
+	ft_strlcpy((*matrix)[i], value_env, new_size + 1);
+	free(value_env);
 }
 
 int	main(void)
@@ -152,13 +261,12 @@ int	main(void)
 	i = 0;
 	init_data(&data);
 	data.envp = get_all_environment();
-	matrix = split_2("echo:$USER", ':'); // ''$HOME''''$HOME''
+	matrix = split_2("echo:'\"$USER\"''\"$HOME\"'", ':');
 	while (matrix[i])
 	{
-		ft_printf("%s\n", matrix[i]);
 		if (ft_strchr(matrix[i], '$'))
 			environment_variation_expansion(i, &matrix, &data);
-		// ft_printf("%s\n", matrix[i]);
+		ft_printf("[[%s]]\n", matrix[i]);
 		i++;
 	}
 	free_matrix(matrix);
