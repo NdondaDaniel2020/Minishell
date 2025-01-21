0245eff9ed8f084b484dc07ebfef46dce539ab96
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2025-01-21 10:44:12 +0100

    Ajuste no diberamento de todos os dados alocados, de free_all_data para free_data

--- src/heredoc_pipe.c
+++ src/heredoc_pipe.c
@@ -12,20 +12,6 @@
 
 #include "minishell.h"
 
-bool	just_space(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] != ' ')
-			return (false);
-		i++;
-	}
-	return (true);
-}
-
 static char	*handle_heredoc_input(t_data *data)
 {
 	char	*line;
@@ -33,40 +19,31 @@ static char	*handle_heredoc_input(t_data *data)
 	while (1)
 	{
 		line = readline("> ");
+		if (g_satatus == 1)
+			return (free_all_data(data), free(line), NULL);
 		if (line == NULL)
 		{
 			ft_putstr_fd("syntax error: unexpected end of file\n",
 				STDERR_FILENO);
-			free_all_data(data);
+			free_data(data);
 			ft_putstr_fd("exit\n", STDERR_FILENO);
 			exit(2);
 		}
-		if (line && just_space(line) == false
+		if (line && all_is_space(line) == false
 			&& (ft_strchr(line, '$')
 				|| ft_strchr(line, '\'')
 				|| ft_strchr(line, '"')))
 			environment_variation_expansion_in_heredoc(&line, data);
-		if (line && just_space(line) == false)
+		if (line && all_is_space(line) == false)
 			return (line);
 		free(line);
 	}
 }
 
-void	heredoc_pipe_fork(t_data *data)
+static void	add_matrix_in_newlist(t_data *data, char **matrix)
 {
-	char		*new_line;
-	char		*aux_chr;
-	char		**matrix;
 	t_new_list	*aux;
 
-	new_line = handle_heredoc_input(data);
-	aux_chr = ft_strjoin(data->command, new_line);
-	add_history(aux_chr);
-	matrix = split_2(new_line, ' ');
-	matrix_space_position_adjustment(&matrix);
-	null_string(&matrix);
-	free(new_line);
-	free(aux_chr);
 	aux = data->list;
 	while (aux)
 	{
@@ -79,4 +56,24 @@ void	heredoc_pipe_fork(t_data *data)
 		aux = aux->next;
 	}
 	ft_lstnew_addback(&data->list, ft_lstnew_new(matrix));
+}
+
+bool	heredoc_pipe_fork(t_data *data)
+{
+	char		*new_line;
+	char		*aux_chr;
+	char		**matrix;
+
+	new_line = handle_heredoc_input(data);
+	if (new_line == NULL)
+		return (false);
+	aux_chr = ft_strjoin(data->command, new_line);
+	add_history(aux_chr);
+	matrix = split_2(new_line, ' ');
+	matrix_space_position_adjustment(&matrix);
+	null_string(&matrix);
+	free(new_line);
+	free(aux_chr);
+	add_matrix_in_newlist(data, matrix);
+	return (true);
 }
