dba1f2ebeb5d726d3aa1f19fa95f1510df1db73f
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2025-01-21 10:49:51 +0100

    Foram add as funcoes character_of_unclosed_quotes has_unclosed_quotes
    A funcao has_unclosed_quotes verifica se tem aspas nao fechado. E character_of_unclosed_quotes pega o caracter de aspas nao fechado

--- src/util_5.c
+++ src/util_5.c
@@ -25,3 +25,72 @@ bool	all_is_space(char *str)
 	}
 	return (true);
 }
+
+bool	is_pipe_heredoc(char *command)
+{
+	int	len;
+
+	len = ft_strlen(command);
+	if (command[len - 1] == '|')
+		return (true);
+	while (len > 0 && command[len - 1] == ' ' )
+		len--;
+	if (command[len - 1] == '|')
+		return (true);
+	else
+		return (false);
+}
+
+bool	has_unclosed_quotes(const char *str)
+{
+	int		i;
+	int		save;
+	char	chr;
+
+	i = 0;
+	save = -1;
+	chr = '\0';
+	while (str[i] != '\0')
+	{
+		if ((str[i] == '\'' || str[i] == '\"') && chr == '\0')
+		{
+			chr = str[i];
+			save = i;
+		}
+		else if (chr != '\0' && str[i] == chr && save != i)
+		{
+			chr = '\0';
+			save = -1;
+		}
+		i++;
+	}
+	if (chr != '\0')
+		return (true);
+	return (false);
+}
+
+char	character_of_unclosed_quotes(const char *str)
+{
+    int		i;
+	int		save;
+	char	chr;
+
+    i = 0;
+	save = -1;
+	chr = '\0';
+    while (str[i] != '\0')
+    {
+		if ((str[i] == '\'' || str[i] == '\"') && chr == '\0')
+		{
+			chr = str[i];
+			save = i;
+		}
+		else if (chr != '\0' && str[i] == chr && save != i)
+		{
+			chr = '\0';
+			save = -1;
+		}
+        i++;
+    }
+	return (chr);
+}
