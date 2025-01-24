0224ac7e5f3deb859e2361fc1ab36a9123e0b624
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2025-01-24 09:29:52 +0100

    Ajuste de echo

--- src/echo.c
+++ src/echo.c
@@ -9,19 +9,21 @@
 /*   Updated: 2024/11/06 10:05:31 by nmatondo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/* modificado*/
+
 #include "minishell.h"
 
-int	echo(t_new_list *aux, t_data *data)
+int echo(t_new_list *aux, t_data *data)
 {
-	int		i1;
-	int		i2;
+	int	i1;
+	int	i2;
+	int	len;
 
 	if (echo_is_empty(aux))
 		return (change_environment_variables_question_mark(0, data));
 	i1 = 0;
 	while (!ft_strncmp(aux->content[i1], "echo", ft_strlen(aux->content[i1])))
 		i1++;
+	len = len_matrix(aux->content);
 	traverse_n(&i1, data);
 	while (aux->content[i1])
 	{
@@ -31,7 +33,7 @@ int	echo(t_new_list *aux, t_data *data)
 			ft_putchar_fd(aux->content[i1][i2], 1);
 			i2++;
 		}
-		if (aux->content[i1 + 1] && data->space)
+		if (i1 < len)
 			ft_putchar_fd(' ', 1);
 		i1++;
 	}
