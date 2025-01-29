d9e85a317421973cfe41dafc24b63cb5a63ab224
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2025-01-29 21:33:08 +0100

    Ajuste de pwd

--- src/pwd.c
+++ src/pwd.c
@@ -12,37 +12,10 @@
 
 #include "minishell.h"
 
-static bool	error_pwd(t_new_list *aux)
-{
-	int	i;
-	int	len;
-
-	i = 0;
-	len = 0;
-	while (aux->content[i])
-	{
-		if (ft_strlen(aux->content[i]) == 0)
-		{
-			i++;
-			continue ;
-		}
-		if (len > 0 && ft_strlen(aux->content[i]) > 0)
-		{
-			write(2, "pwd: too many arguments\n", 24);
-			return (true);
-		}
-		i++;
-		len++;
-	}
-	return (false);
-}
-
 int	pwd(t_new_list *aux, t_data *data)
 {
 	char	*cwd;
 
-	if (error_pwd(aux))
-		return (change_environment_variables_question_mark(1, data));
 	cwd = ft_calloc(5048, sizeof(char));
 	if (!cwd)
 		return (change_environment_variables_question_mark(2, data));
