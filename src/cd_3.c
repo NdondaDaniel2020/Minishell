9a97f728ce6a71c0663bde525a572774c297b27a
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2025-01-29 15:55:14 +0100

    Ajuste no cd
    Msg add quando tenta entrar em uma pasta sem permicao

--- src/cd_3.c
+++ src/cd_3.c
@@ -19,10 +19,10 @@ void	chdir_(char *dir)
 	status = chdir(dir);
 	if (status != 0)
 	{
-		ft_putstr_fd("cd: p", 2);
-		ft_putstr_fd(strerror(errno) + 1, 2);
-		ft_putstr_fd(": ", 2);
+		ft_putstr_fd("cd: ", 2);
 		ft_putstr_fd(dir, 2);
+		ft_putstr_fd(": ", 2);
+		ft_putstr_fd(strerror(errno), 2);
 		ft_putstr_fd("\n", 2);
 	}
 }
